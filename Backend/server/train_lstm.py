d10589bc559ef1a182be3ceebf42976a4d626257
Pacman <asadrahman225@gmail.com>
2023-06-18 14:18:01 +0500

    Update train_lstm.py

--- Backend/server/train_lstm.py
+++ Backend/server/train_lstm.py
@@ -1,43 +1,27 @@
-
-# Import the required libraries
 import pandas as pd
-import nlpaug.augmenter.word as naw
-import nlpaug.flow as naf
-from transformers import pipeline
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Bidirectional, Dense, Dropout, Embedding
-from tensorflow.keras.optimizers import SGD
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from sklearn.model_selection import train_test_split
+from nltk.corpus import wordnet as wn
+import random
 
+# Function to perform synonym replacement
+def replace_synonyms(sentence):
+    words = sentence.split()
+    augmented_sentence = []
+    for word in words:
+        synsets = wn.synsets(word)
+        if synsets:
+            synonyms = [syn.lemmas()[0].name() for syn in synsets]
+            synonym = random.choice(synonyms)
+            augmented_sentence.append(synonym)
+        else:
+            augmented_sentence.append(word)
+    return ' '.join(augmented_sentence)
 
 df = pd.read_excel('BankFAQs.xlsx')
-# Load the T5 model and tokenizer
-model_name = 't5-base'
-tokenizer = T5Tokenizer.from_pretrained(model_name)
-model = T5ForConditionalGeneration.from_pretrained(model_name)
-
-# Function to generate paraphrases using back-translation
-def generate_paraphrase(text):
-    # Translate to a different language
-    translated = model.generate(input_ids=tokenizer.encode(text, return_tensors='pt'), 
-                               max_length=100, 
-                               num_return_sequences=1,
-                               early_stopping=True,
-                               decoder_start_token_id=model.config.pad_token_id)
-    
-    # Translate back to the original language
-    translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
-    back_translated = model.generate(input_ids=tokenizer.encode(translated_text, return_tensors='pt'), 
-                                     max_length=100, 
-                                     num_return_sequences=1,
-                                     early_stopping=True,
-                                     decoder_start_token_id=model.config.pad_token_id)
-    
-    # Return the back-translated paraphrase
-    paraphrase = tokenizer.decode(back_translated[0], skip_special_tokens=True)
-    return paraphrase
 
 # Apply data augmentation to create additional training examples
 augmented_questions = []
@@ -47,11 +31,12 @@ for question, label in zip(df['Questions'], df['Responses']):
     augmented_questions.append(question)
     augmented_labels.append(label)
     
-    paraphrase = generate_paraphrase(question)
+    paraphrase = replace_synonyms(question)
     augmented_questions.append(paraphrase)
     augmented_labels.append(label)
 
 # Tokenize and pad sequences
+tokenizer = Tokenizer(num_words=10000)
 tokenizer.fit_on_texts(augmented_questions)
 sequences = tokenizer.texts_to_sequences(augmented_questions)
 padded_sequences = pad_sequences(sequences, maxlen=100, padding='post', truncating='post')
@@ -60,10 +45,9 @@ padded_sequences = pad_sequences(sequences, maxlen=100, padding='post', truncati
 labels = pd.get_dummies(augmented_labels).values
 
 # Split the data into training and validation sets
-from sklearn.model_selection import train_test_split
 X_train, X_val, y_train, y_val = train_test_split(padded_sequences, labels, test_size=0.2, random_state=42)
 
-# Define and compile the model (using the architecture from the previous example)
+# Define and compile the model
 model = Sequential([
     Embedding(input_dim=10000, output_dim=64, input_length=100),
     Bidirectional(LSTM(256, return_sequences=True)),
